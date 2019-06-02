fce8746a7952576225430acce200c0890f926f1e
fancyzhong <fancyzhong@tencent.com>
2019-06-02 16:06:30 +0800

    release 1.1.20

--- es/cc-context/index.js
+++ es/cc-context/index.js
@@ -206,7 +206,7 @@ const ccContext = {
   refs,
   info: {
     startupTime: Date.now(),
-    version: '1.2.19',
+    version: '1.2.20',
     author: 'fantasticsoul',
     emails: ['624313307@qq.com', 'zhongzhengkai@gmail.com'],
     tag: 'xenogear',
