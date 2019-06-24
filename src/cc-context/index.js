aafde97b1e29d7cedbb0cff11f40cb17a1cce5b6
fancyzhong <fancyzhong@tencent.com>
2019-06-24 17:15:12 +0800

    优化CcFragment引用解除逻辑

--- src/cc-context/index.js
+++ src/cc-context/index.js
@@ -238,7 +238,7 @@ const ccContext = {
   refs,
   info: {
     startupTime: Date.now(),
-    version: '1.2.35',
+    version: '1.2.36',
     author: 'fantasticsoul',
     emails: ['624313307@qq.com', 'zhongzhengkai@gmail.com'],
     tag: 'xenogear',
