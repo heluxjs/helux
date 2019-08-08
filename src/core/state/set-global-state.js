cb8c3e4df112f35ff7d92fb1ddb66e4778c6bda1
fancyzhong <fancyzhong@tencent.com>
2019-08-08 09:28:53 +0800

    optimize: 保持cc.setGlobalState和实例的一致

--- src/core/state/set-global-state.js
+++ src/core/state/set-global-state.js
@@ -5,10 +5,10 @@ import pickOneRef from '../ref/pick-one-ref';
  * if you are sure the input state is really belong to global state, call cc.setGlobalState,
  * note! cc will filter the input state to meet global state shape and only pass the filtered state to global module
  */
-export default function (state, delayMs = -1, throwError = false) {
+export default function (state, cb, delay, idt, throwError = false) {
   try {
     const ref = pickOneRef();
-    ref.ctx.setGlobalState(state, delayMs);
+    ref.ctx.setGlobalState(state, cb, delay, idt);
   } catch (err) {
     if (throwError) throw err;
     else util.justWarning(err.message);
