951551a44bccde3afad437ad00f0a66478971ad7
fancyzhong <fancyzhong@tencent.com>
2019-08-13 09:44:55 +0800

    mod: renanme incorect spelling reanderKeyrederKey

--- src/core/state/set-state.js
+++ src/core/state/set-state.js
@@ -4,7 +4,7 @@ import pickOneRef from '../ref/pick-one-ref';
 export default function (module, state, delay = -1, renderKey, skipMiddleware) {
   try {
     const ref = pickOneRef(module);
-    const option = { ccKey: '[[top api:cc.setState]]', module, delay, renderKeyrenderKey, skipMiddleware };
+    const option = { ccKey: '[[top api:cc.setState]]', module, delay, renderKey, skipMiddleware };
     ref.ctx.changeState(state, option);
   } catch (err) {
     strictWarning(err);
