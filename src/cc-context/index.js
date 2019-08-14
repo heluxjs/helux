dccc839ffd4f32bfe889ef9030d3ecd4ab63e8c7
fancyzhong <fancyzhong@tencent.com>
2019-08-14 13:54:47 +0800

    release: v1.5.14

--- src/cc-context/index.js
+++ src/cc-context/index.js
@@ -212,7 +212,7 @@ const ccContext = {
   refs,
   info: {
     startupTime: Date.now(),
-    version: '1.5.13',
+    version: '1.5.14',
     author: 'fantasticsoul',
     emails: ['624313307@qq.com', 'zhongzhengkai@gmail.com'],
     tag: 'destiny',
