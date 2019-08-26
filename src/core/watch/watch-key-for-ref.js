04160fcca0c3df9879ddfed0883247fdd050ae05
fancyzhong <fancyzhong@tencent.com>
2019-08-26 11:20:36 +0800

    optimize: 针对多key依赖,返回正确的newState

--- src/core/watch/watch-key-for-ref.js
+++ src/core/watch/watch-key-for-ref.js
@@ -7,20 +7,24 @@ export default function (refCtx, stateModule, oldState, committedState, isBefore
   let shouldCurrentRefUpdate = true;
   // 触发有stateKey依赖列表相关的watch函数
   const { pickedFns, setted, changed } = pickDepFns(isBeforeMount, 'ref', 'watch', watchDep, stateModule, oldState, committedState, ccUniqueKey);
-  pickedFns.forEach(({ fn, retKey, depKeys }) => {
-    const fnCtx = { retKey, setted, changed, stateModule, refModule, oldState, committedState, refCtx };
-    const fistDepKey = depKeys[0];
 
-    let ret;
-    if (depKeys.length === 1 && fistDepKey !== '*') {
-      ret = fn(committedState[fistDepKey], oldState[fistDepKey], fnCtx, refCtx);
-    } else {
-      ret = fn(committedState, oldState, fnCtx);
-    }
+  if (pickedFns.length) {
+    const newState = Object.assign({}, oldState, committedState);
+    pickedFns.forEach(({ fn, retKey, depKeys }) => {
+      const fnCtx = { retKey, setted, changed, stateModule, refModule, oldState, committedState, refCtx };
+      const fistDepKey = depKeys[0];
 
-    //实例里只要有一个watch函数返回false，就会阻碍当前实例的ui被更新
-    if (ret === false) shouldCurrentRefUpdate = false;
-  });
+      let ret;
+      if (depKeys.length === 1 && fistDepKey !== '*') {
+        ret = fn(committedState[fistDepKey], oldState[fistDepKey], fnCtx, refCtx);
+      } else {
+        ret = fn(newState, oldState, fnCtx);
+      }
+
+      //实例里只要有一个watch函数返回false，就会阻碍当前实例的ui被更新
+      if (ret === false) shouldCurrentRefUpdate = false;
+    });
+  }
 
   return shouldCurrentRefUpdate;
 }
