1b5fb7718a4055efdb5592643ba608411f7530ed
fancyzhong <fancyzhong@tencent.com>
2019-11-15 18:04:20 +0800

    optimize: optimize guess-duplicate

--- src/cc-context/index.js
+++ src/cc-context/index.js
@@ -237,7 +237,7 @@ const ccContext = {
   refs,
   info: {
     startupTime: Date.now(),
-    version: '1.5.40',
+    version: '1.5.41',
     author: 'fantasticsoul',
     emails: ['624313307@qq.com', 'zhongzhengkai@gmail.com'],
     tag: 'destiny',
--- src/core/base/guess-duplicate.js
+++ src/core/base/guess-duplicate.js
@@ -1,26 +1,57 @@
 import { ERR } from '../../support/constant';
 import ccContext from '../../cc-context';
 
-const keyWord = 'at Object.configure';
+const keyWord = '.checkModuleNameAndState';
 
 function getDupLocation(errStack) {
   if (!errStack) errStack = '';
 
+  /** stack may like this: at CodeSandbox
+  Error: module name duplicate! --verbose-info: module[SetupDemo]
+    at makeError (https://xvcej.csb.app/node_modules/concent/src/support/util.js:128:15)
+    at checkModuleName (https://xvcej.csb.app/node_modules/concent/src/core/checker/index.js:71:15)
+>>  at Object.checkModuleNameAndState (https://xvcej.csb.app/node_modules/concent/src/core/checker/index.js:90:3)
+    at _default (https://xvcej.csb.app/node_modules/concent/src/core/state/init-module-state.js:25:13)
+    at _default (https://xvcej.csb.app/node_modules/concent/src/api/configure.js:96:35)
+>>  at evaluate (https://xvcej.csb.app/src/pages/SetupDemo/model/index.js:13:24)
+    at Jn (https://codesandbox.io/static/js/sandbox.fb6f2fde.js:1:146799)
+    at e.value (https://codesandbox.io/static/js/sandbox.fb6f2fde.js:1:162063)
+    at e.value (https://codesandbox.io/static/js/sandbox.fb6f2fde.js:1:202119)
+    at t (https://codesandbox.io/static/js/sandbox.fb6f2fde.js:1:161805)
+    ...
+
+  or: at local web-dev-server
+
+  Error: module name duplicate! --verbose-info: module[batchAddGroup]
+    at makeError (http://localhost:3001/static/js/main.chunk.js:20593:17)
+    at checkModuleName (http://localhost:3001/static/js/main.chunk.js:17256:15)
+>>  at Module.checkModuleNameAndState (http://localhost:3001/static/js/main.chunk.js:17273:3)
+    at http://localhost:3001/static/js/main.chunk.js:19804:106
+    at Object.configure (http://localhost:3001/static/js/main.chunk.js:13750:80)
+>>  at Module../src/components/layer/BatchOpGroup/model/index.js (http://localhost:3001/static/js/main.chunk.js:8374:55)
+    at __webpack_require__ (http://localhost:3001/static/js/bundle.js:782:30)
+    at fn (http://localhost:3001/static/js/bundle.js:150:20)
+   */
   const arr = errStack.split('\n');
   const len = arr.length;
   let locationStr = '';
   for (let i = 0; i < len; i++) {
     const strPiece = arr[i];
+    if (i > strPickLimit) break;
+    locationStr += strPiece;
+
     if (strPiece.includes(keyWord)) {
+      const callConfigureIdx = i + 3;// 向下3句就是调用处
       // 这句话是具体调用configure的地方
-      // "    at Object.configure (http://localhost:3001/static/js/main.chunk.js:13750:80)"
-      const arr2 = strPiece.split(':');
+      // at Module../src/components/layer/BatchOpGroup/model/index.js (http://localhost:3001/static/js/main.chunk.js:8374:55)
+      const targetStrPiece = arr[callConfigureIdx];
+      const arr2 = targetStrPiece.split(':');
       const lastIdx = arr2.length - 1;
       const locationStrArr = [];
       arr2.forEach((str, idx) => {
         if (idx !== lastIdx) locationStrArr.push(str);
       })
-      // "    at Object.configure (http://localhost:3001/static/js/main.chunk.js:13750)"
+      // at Module../src/components/layer/BatchOpGroup/model/index.js (http://localhost:3001/static/js/main.chunk.js:8374
       locationStr = locationStrArr.join(':');
       break;
     }
