14091ff611a196a5153312fb7e55e4f8c21c592d
fancyzhong <fancyzhong@tencent.com>
2019-11-20 13:22:48 +0800

    release:1.5.48

--- dist/concent.js
+++ dist/concent.js
@@ -3386,7 +3386,7 @@
           ccUniqueKey = refCtx.ccUniqueKey;
 
       if (hasSyncCb) {
-        if (extraState) return changeRefState(extraState, {
+        if (extraState) changeRefState(extraState, {
           calledBy: SYNC,
           ccKey: ccKey,
           ccUniqueKey: ccUniqueKey,
@@ -3394,6 +3394,7 @@
           renderKey: ccrkey,
           delay: ccdelay
         }, ref);
+        return;
       }
 
       var fullState = targetModule !== refModule ? getState$2(targetModule) : ref.state;
@@ -3401,7 +3402,7 @@
       var _extractStateByCcsync = extractStateByCcsync(ccsync, value, ccint, fullState, mockE.isToggleBool),
           state = _extractStateByCcsync.state;
 
-      return changeRefState(state, {
+      changeRefState(state, {
         calledBy: SYNC,
         ccKey: ccKey,
         ccUniqueKey: ccUniqueKey,
@@ -3412,13 +3413,14 @@
     } else {
       //调用自己的setState句柄触发更新，key可能属于local的，也可能属于module的
       if (hasSyncCb) {
-        if (extraState) return ref.setState(extraState, null, ccrkey, ccdelay);
+        if (extraState) ref.setState(extraState, null, ccrkey, ccdelay);
+        return;
       }
 
       var _extractStateByCcsync2 = extractStateByCcsync(ccsync, value, ccint, ref.state, mockE.isToggleBool),
           _state = _extractStateByCcsync2.state;
 
-      return ref.setState(_state, null, ccrkey, ccdelay);
+      ref.setState(_state, null, ccrkey, ccdelay);
     }
   }
 
