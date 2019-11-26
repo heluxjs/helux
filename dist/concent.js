646115a5304d433657631f7b3d43b95ef865d845
fancyzhong <fancyzhong@tencent.com>
2019-11-26 18:27:23 +0800

    release: v1.5.60

--- dist/concent.js
+++ dist/concent.js
@@ -906,7 +906,7 @@
     refs: refs,
     info: {
       startupTime: Date.now(),
-      version: '1.5.59',
+      version: '1.5.60',
       author: 'fantasticsoul',
       emails: ['624313307@qq.com', 'zhongzhengkai@gmail.com'],
       tag: 'destiny'
