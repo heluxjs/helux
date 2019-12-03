43839d1996ae1daa11f871ce76ab7668b521786f
fancyzhong <fancyzhong@tencent.com>
2019-12-03 15:43:04 +0800

    fix: rename ref.initState to ctx.initState in build-ref-ctx

--- src/cc-context/index.js
+++ src/cc-context/index.js
@@ -240,7 +240,7 @@ const ccContext = {
   refs,
   info: {
     startupTime: Date.now(),
-    version: '1.5.67',
+    version: '1.5.68',
     author: 'fantasticsoul',
     emails: ['624313307@qq.com', 'zhongzhengkai@gmail.com'],
     tag: 'destiny',
