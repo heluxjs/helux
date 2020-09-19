8d536dd518c86bfb9c0671cb0cae9301b2cc3164
fancyzhong <fancyzhong@tencent.com>
2020-09-19 21:41:14 +0800

    chore: bump node-fetch

--- dist/concent.js
+++ dist/concent.js
@@ -3104,7 +3104,7 @@
       packageLoadTime: Date.now(),
       firstStartupTime: '',
       latestStartupTime: '',
-      version: '2.9.29',
+      version: '2.9.30',
       author: 'fantasticsoul',
       emails: ['624313307@qq.com', 'zhongzhengkai@gmail.com'],
       tag: 'glaxy'
