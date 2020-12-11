15c3e67050786fb63147acc1c19520a47d18b42d
fancyzhong <fancyzhong@tencent.com>
2020-12-11 12:41:20 +0800

    release: v2.10.5

--- src/cc-context/index.js
+++ src/cc-context/index.js
@@ -246,7 +246,7 @@ const ccContext = {
     packageLoadTime: Date.now(),
     firstStartupTime: '',
     latestStartupTime: '',
-    version: '2.10.4',
+    version: '2.10.5',
     author: 'fantasticsoul',
     emails: ['624313307@qq.com', 'zhongzhengkai@gmail.com'],
     tag: 'glory',
