cbbda3945559d9224ae435f9065373088239287f
fantasticsoul <zhongzhengkai@gmail.com>
2023-11-18 22:07:35 +0800

    build(helux-preact 3.0.9): 调整导出方式

--- packages/helux-fre/src/index.ts
+++ packages/helux-fre/src/index.ts
@@ -14,19 +14,16 @@ export const {
   createShared,
   // derive api
   derive,
-  deriveAsync,
   deriveAtom,
-  deriveAtomAsync,
   runDerive,
+  runDeriveAsync,
   // watch api
   watch,
   // hooks api
   useAtom,
   useShared,
   useDerived,
-  useDerivedAsync,
   useDerivedAtom,
-  useDerivedAtomAsync,
   useWatch,
   useGlobalId,
   useService,
@@ -77,6 +74,5 @@ export const {
   addMiddleware,
   addPlugin,
   EVENT_NAME,
-  WAY,
   LOADING_MODE,
 } = api;
